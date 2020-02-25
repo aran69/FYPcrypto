d67612568b73afc9ae0a88430d72a8dd74f07dbd
ARAN LEWIS SMEALLIE <u190077@CS4-80>
2020-02-25 15:06:58 +0000

    Added more notes to Log.txt

--- NTRU.java
+++ NTRU.java
@@ -2,19 +2,25 @@ import java.util.*;
 
 public class NTRU{
 	public static void main (String[] args){
-		switch(args[0]){
-			case "encrypt": System.out.print(encrypt(args[1],args[2]));
-			break;
+		try{
+			switch(args[0]){
+				case "encrypt": System.out.print(encrypt(args[1],args[2]));
+				break;
 
-			case "decrypt": System.out.print(decrypt(args[1],args[2]));
-			break;
+				case "decrypt": System.out.print(decrypt(args[1],args[2]));
+				break;
 
-			case "keygen": System.out.print(keygen(args[1]));
-			break;
+				case "keygen": System.out.print(keygen(args[1]));
+				break;
 
-			default : System.out.println("Please append valid action and parameters to this class' call, i.e \"encrypt\", \"decrypt\" or \"keygen\". ");
-			break;
+				default : System.out.println("Please append valid action and parameters to this class' call, i.e \"encrypt\", \"decrypt\" or \"keygen\". ");
+				break;
+			}
 		}
+		catch(Exception e){
+			System.out.println("Please call this class with either encrypt, decrypt or keygen as args, followed by additional args \n encrypt/decrypt requires two additional args, a key and a message \n keygen requires one additional argument, a seed.");
+		}
+		
 	}	
 
 	public static int[] encrypt(String key, String message){
